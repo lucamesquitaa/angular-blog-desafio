304e982e2d76ab8f251da3d69d947672960fa967
Luca Mesquita <luca.mesquita@predito.com.br>
2022-12-11 15:50:04 -0300

    projeto base criado

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent {
+  @Input()
+  smallPhoto: string = ""
+  @Input()
+  smallTitle: string = ""
+  @Input()
+  id:string = "0"
+
+}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"4",
+        "photo": "https://meups.com.br/wp-content/uploads/2022/09/Homem-de-Ferro-900x503.jpg",
+        "title":"NOVO HOMEM DE FERRO",
+        "description":"Marvel anuncia novo filme homem de ferro"
+    },
+    {
+        "id":"1",
+        "photo": "https://www.ofuxico.com.br/wp-content/uploads/2022/11/wandinha-jenny-ortega-seria-em-poster-serie-netflix.jpg",
+        "title":"Conheça Wandinha: série que está bombando entre os jovens",
+        "description":""
+    },
+    {
+        "id":"2",
+        "photo": "https://streamingflix.com.br/wp-content/uploads/2022/11/1899-netflix-dark-fragman-yeni.jpeg",
+        "title":"Série 1899 faz sucesso entre entusiastas de DARK",
+        "description":""
+    },
+    {
+        "id":"3",
+        "photo": "https://www.taglivros.com/blog/wp-content/uploads/2021/09/Prancheta-1-8.png",
+        "title":"Quais as melhores séries da NETFLIX de 2022? Confira a lsta!",
+        "description":""
+    }
+]
